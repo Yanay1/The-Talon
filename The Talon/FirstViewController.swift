8011c49ae4e4f3a834d2e275d8a8ed92c91f6542
Yanay Rosen <yanayrosen@gmail.com>
2015-12-24 12:15:01 -0500

    Initial Commit

---
+++ The Talon/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  The Talon
+//
+//  Created by Yanay Rosen on 12/24/15.
+//  Copyright © 2015 Yanay Rosen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
